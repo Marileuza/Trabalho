09caaccd03ab969b8812c32f3015192426892271
Marileuza <119111599+Marileuza@users.noreply.github.com>
2023-11-13 21:13:00 -0300

    Add files via upload

---
+++ cad.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html  lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viwport" content="width=device-width, initial-scale=1">
+	<title>Interação com Formulários</title>
+	<link rel="stylesheet" type="text/css" href="style01.css">
+</head>
+</head>
+<body>
+	<header>
+		<div class="div1">
+		<div class="div02">
+		<h1 style="color: #372991;">Resultado do processamento</h1>
+		</div>
+
+	</div>
+	</header>
+	<main>
+		<?php
+		$nome = $_GET['nome']?? "Digite o nome";
+		$sobrenome = $_GET['sobrenome']?? "Desconhecido";
+		echo"<p> É um prazer te conhecer, <strong> $nome $sobrenome!</strong> Este é o meu site!";
+		?>
+		<p><a href="javascript:history.go(-1)">Voltar para página anterior</a></p>
+	</main>
+
+</body>
+</html>
